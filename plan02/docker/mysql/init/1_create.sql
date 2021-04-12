061abb20aeca630613ca8bb32c0f259bd2b80b52
so-heee <so.heee.mochi@gmail.com>
2021-04-13 00:23:51 +0900

    add plan02

---
+++ plan02/docker/mysql/init/1_create.sql
@@ -0,0 +1,7 @@
+USE sample;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    PRIMARY KEY (id));
+);
---
+++ plan02/go.mod
@@ -0,0 +1,5 @@
+module github.com/so-heee/graphql-example/plan02
+
+go 1.16
+
+require github.com/99designs/gqlgen v0.13.0 // indirect
